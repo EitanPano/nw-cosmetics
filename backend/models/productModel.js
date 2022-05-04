9bf43c138c7a7f851660b019db3abefb642bf0f7
Eitan Pano <eitanpano95@gmail.com>
2022-05-05 00:10:58 +0300

    added security optimizition

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,15 +1,15 @@
-// const reviewModel = {
-//     type: 'object',
-//     properties: {
-//         userId: { type: 'string' },
-//         productId: { type: 'string' },
-//         title: { type: 'string' },
-//         rating: { type: 'number' },
-//         description: { type: 'string' },
-//         createdAt: { type: 'number' }
-//     },
-//     required: []
-// };
+const reviewModel = {
+    type: 'object',
+    properties: {
+        userId: { type: 'string' },
+        productId: { type: 'string' },
+        title: { type: 'string' },
+        rating: { type: 'number' },
+        description: { type: 'string' },
+        createdAt: { type: 'number', default: Date.now() },
+    },
+    required: ['userId', 'productId', 'title', 'rating'],
+};
 
 const productModel = {
     type: 'object',
@@ -24,8 +24,9 @@ const productModel = {
         inStockCount: { type: 'number', default: 0 },
         rating: { type: 'number', default: 0 },
         reviewCount: { type: 'number', default: 0 },
+        createdAt: { type: 'number', default: Date.now() },
     },
     required: ['name', 'image', 'description', 'brand', 'category', 'price'],
 };
 
-export { productModel as productSchema };
+export { productModel as productSchema, reviewModel as reviewSchema };
