06d5289c6f25db18c65a67589f85fdd106c9b61d
Eitan Pano <eitanpano95@gmail.com>
2022-05-11 22:56:18 +0300

    removed console.logs

--- frontend/src/store/product/reducer.js
+++ frontend/src/store/product/reducer.js
@@ -8,19 +8,19 @@ const initialState = {
 export const productReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'LOAD_PRODUCTS':
-            console.log('action: ', action.type);
+            // console.log('action: ', action.type);
             return { ...state, products: [...action.products], error: null };
 
         case 'SET_PRODUCT':
-            console.log('action: ', action.type);
-            return { ...state, selectedProduct: action.product };
+            // console.log('action: ', action.type);
+            return { ...state, selectedProduct: action.product, error: null };
 
         case 'ADD_PRODUCT':
-            console.log('action: ', action.type);
+            // console.log('action: ', action.type);
             return { ...state, products: [...state.products, action.product] };
 
         case 'UPDATE_PRODUCT':
-            console.log('action: ', action.type);
+            // console.log('action: ', action.type);
             return {
                 ...state,
                 products: state.products.map((product) =>
@@ -28,14 +28,14 @@ export const productReducer = (state = initialState, action) => {
             };
 
         case 'REMOVE_PRODUCT':
-            console.log('action: ', action.type);
+            // console.log('action: ', action.type);
             return { 
                 ...state,
                 products: state.products.filter(product => product._id === action.productId)
             };
 
         case 'SET_ERROR':
-            console.log('action: ', action)
+            // console.log('action: ', action)
             return { ...state, error: action.error }
 
         default:
